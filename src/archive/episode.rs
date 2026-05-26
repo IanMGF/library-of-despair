bcdd4226d5c38dbe052496231e7564088a091ff9
IanMGF <ianmarcosgomesefreitas@gmail.com>
2026-05-26 02:07:48 -0300

    feat: add file recognition for multiple episodes

---
+++ src/archive/episode.rs
@@ -0,0 +1,43 @@
+use std::{fs::File, path::PathBuf};
+
+use serde::{Deserialize, Serialize};
+
+use crate::{archive::content::Content, assignments::OwnedAssignmentSet, cast::Cast};
+
+#[derive(Serialize, Deserialize)]
+pub struct Episode {
+    pub name: String,
+    pub id: String,
+    pub season_name: String,
+    pub season_id: String,
+    pub youtube_url: String,
+    pub number: u8,
+    pub wiki_url: Option<String>,
+}
+
+impl Episode {
+    pub fn get_episodes_list() -> std::io::Result<Vec<Episode>> {
+        let file: File = File::open(format!("documents/episodes.yaml"))?;
+        let episodes: Vec<Episode> = yaml_serde::from_reader(file).unwrap();
+        Ok(episodes)
+    }
+
+    pub fn get_content(&self) -> std::io::Result<Content> {
+        let id: &str = self.id.as_str();
+        let path: PathBuf = PathBuf::from(format!("documents/{id}/content.txt"));
+        Content::from_file(path)
+    }
+
+    pub fn get_cast(&self) -> std::io::Result<Cast> {
+        let id: &str = self.id.as_str();
+        let file: File = File::open(format!("documents/{id}/content.txt"))?;
+        let cast: Cast = yaml_serde::from_reader(file).unwrap();
+        Ok(cast)
+    }
+
+    pub fn get_assignments(&self) -> std::io::Result<OwnedAssignmentSet> {
+        let id: &str = self.id.as_str();
+        let path: PathBuf = PathBuf::from(format!("documents/{id}/assignment.csv"));
+        OwnedAssignmentSet::from_file(path)
+    }
+}
--- src/search/mod.rs
+++ src/search/mod.rs
@@ -1,13 +1,16 @@
 pub mod score;
 
-use std::path::PathBuf;
+use std::sync::Arc;
 
 use axum::{Json, extract::Query};
 use backend::{
-    archive::content::Content,
-    assignments::{AssignmentSet, AssignmentUnit, OwnedAssignmentSet},
+    archive::{content::Content, episode::Episode},
+    assignments::{AssignmentSet, AssignmentUnit},
 };
-use rayon::iter::{ParallelBridge, ParallelIterator};
+use rayon::{
+    iter::{ParallelBridge, ParallelExtend, ParallelIterator},
+    slice::ParallelSliceMut,
+};
 use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize)]
@@ -32,36 +35,50 @@ type Timestamp = i64;
 #[derive(Deserialize, Serialize)]
 pub(crate) struct Line {
     time: Timestamp,
-    speakers: Vec<String>,
-    text: String,
+    speakers: Vec<Arc<str>>,
+    text: Arc<str>,
 }
 
 #[derive(Deserialize, Serialize)]
 pub(crate) struct Moment {
     timestamp: String,
-    episode_name: String,
+    episode_name: Arc<str>,
     episode_number: u8,
-    season_name: String,
-    thumbnail_url: String,
+    season_name: Arc<str>,
+    thumbnail_url: Arc<str>,
     youtube_url: String,
 }
 
 pub(crate) async fn search(
     Query(SearchParams { query }): Query<SearchParams>,
 ) -> Json<SearchResult> {
-    let Content(lines) = Content::from_file(PathBuf::from("documents/content.txt")).unwrap();
-    let assignment_set =
-        OwnedAssignmentSet::from_file(PathBuf::from("documents/assignment.csv")).unwrap();
+    let episodes: Vec<Episode> = Episode::get_episodes_list().unwrap();
+
+    let mut results: Vec<SearchResultItem> = vec![];
+
+    for episode in episodes {
+        search_in_episode(&episode, query.as_str(), &mut results);
+    }
+    results.par_sort_by(|a, b| b.score.total_cmp(&a.score));
+    results.truncate(1000);
+    Json(SearchResult(results))
+}
+
+fn search_in_episode(ep: &Episode, query: &str, result_vec: &mut Vec<SearchResultItem>) {
+    let Content(lines) = ep.get_content().unwrap();
+    let assignment_set = ep.get_assignments().unwrap();
 
     let borrowed_assignment_set = assignment_set.into();
     let map_entry_closure = |entry: (usize, &str, f64)| {
         map_entry_to_item(
+            ep,
             (entry.0, entry.2),
             &borrowed_assignment_set,
             lines.as_slice(),
         )
     };
-    let mut results: Vec<SearchResultItem> = lines
+
+    let results = lines
         .iter()
         .enumerate()
         .par_bridge()
@@ -72,15 +89,12 @@ pub(crate) async fn search(
                 score::attribute_score(&query, line.as_str()),
             )
         })
-        .map(map_entry_closure)
-        .collect();
-
-    results.sort_by(|a, b| b.score.total_cmp(&a.score));
-    results.truncate(413);
-    Json(SearchResult(results))
+        .map(map_entry_closure);
+    result_vec.par_extend(results);
 }
 
 fn map_entry_to_item(
+    ep: &Episode,
     (i, score): (usize, f64),
     &AssignmentSet(ref assignments_vec): &AssignmentSet,
     lines: &[String],
@@ -88,18 +102,18 @@ fn map_entry_to_item(
     let line_before = if i > 0 {
         Some(line_from_assignment_and_text(
             &assignments_vec[i - 1],
-            lines[i - 1].clone(),
+            lines[i - 1].as_str().into(),
         ))
     } else {
         None
     };
 
-    let line: Line = line_from_assignment_and_text(&assignments_vec[i], lines[i].clone());
+    let line: Line = line_from_assignment_and_text(&assignments_vec[i], lines[i].as_str().into());
 
     let line_after = if i < lines.len() - 1 {
         Some(line_from_assignment_and_text(
             &assignments_vec[i + 1],
-            lines[i + 1].clone(),
+            lines[i + 1].as_str().into(),
         ))
     } else {
         None
@@ -118,14 +132,11 @@ fn map_entry_to_item(
     // TODO: Add episode-tracking file
     let moment: Moment = Moment {
         timestamp: timestamp_str,
-        episode_name: String::from("Episódio 1"),
-        episode_number: 1,
-        season_name: String::from("Ordem Paranormal RPG"),
-        thumbnail_url: String::new(),
-        youtube_url: format!(
-            "https://www.youtube.com/watch?v=uplnCYc0fDg&t={}s",
-            timestamp.num_seconds()
-        ),
+        episode_name: ep.name.as_str().into(),
+        episode_number: ep.number,
+        season_name: ep.season_name.as_str().into(),
+        thumbnail_url: "".into(),
+        youtube_url: format!("{}&t={}s", ep.youtube_url, timestamp.num_seconds()),
     };
 
     SearchResultItem {
@@ -137,9 +148,13 @@ fn map_entry_to_item(
     }
 }
 
-fn line_from_assignment_and_text(assignment: &AssignmentUnit, text: String) -> Line {
+fn line_from_assignment_and_text(assignment: &AssignmentUnit, text: Arc<str>) -> Line {
     let AssignmentUnit { time, assignments } = assignment;
-    let speakers: Vec<_> = assignments.iter().map(ToString::to_string).collect();
+    let speakers: Vec<_> = assignments
+        .iter()
+        .map(String::as_str)
+        .map(Arc::from)
+        .collect();
     let time = *time;
     Line {
         speakers,
