258820ad58ba32003d06fb30520996cff8f97478
IanMGF <ianmarcosgomesefreitas@gmail.com>
2026-05-27 10:56:49 -0300

    feat: make search score do simple addition

--- src/search/score/mod.rs
+++ src/search/score/mod.rs
@@ -8,7 +8,7 @@ trait ScoringLayer {
 
 pub fn attribute_score(query: &str, line: &str) -> f64 {
     let mut score = 0f64;
-    score = f64::max(score, fuzzy::FuzzScore::attribute_score(query, line));
-    score = f64::max(score, contains::ContainsScore::attribute_score(query, line));
+    score += fuzzy::FuzzScore::attribute_score(query, line);
+    score += contains::ContainsScore::attribute_score(query, line);
     score
 }
