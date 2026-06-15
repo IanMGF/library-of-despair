939c515c732a894d6c2c5a2ff15bd82b7e42d061
IanMGF <ianmarcosgomesefreitas@gmail.com>
2026-06-15 18:12:16 -0300

    feat: add /issue route with post and get

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,35 @@
+mod issue;
 mod search;
-use std::sync::Arc;
+use std::{sync::Arc, time::Duration};
 
-use axum::{Router, routing::get};
+use axum::{
+    Router,
+    routing::{get, post},
+};
 use backend::archive::episode::{Episode, EpisodeInfo};
+use dotenv_codegen::dotenv;
+use sqlx::postgres::PgPoolOptions;
 
 #[tokio::main]
 async fn main() {
+    const DB_URL: &str = dotenv!("DATABASE_URL");
+
+    // Connection pool for database connections
+    let pool = PgPoolOptions::new()
+        .max_connections(50)
+        .acquire_timeout(Duration::from_secs(3))
+        .idle_timeout(Duration::from_secs(10))
+        .connect(DB_URL)
+        .await;
+
+    let pool = match pool {
+        Ok(p) => p,
+        Err(err) => panic!("Erro abrindo pool de conexões com o banco: {err}"),
+    };
+
+    let pool_arc = Arc::new(pool);
+
+    // Load episode information into main memory
     let episodes: Vec<Episode> = EpisodeInfo::get_episodes_list()
         .unwrap()
         .into_iter()
@@ -14,7 +38,11 @@ async fn main() {
 
     let app = Router::new()
         .route("/search", get(search::search))
-        .with_state(Arc::from(episodes));
+        .with_state(Arc::from(episodes))
+        .route("/issue", post(issue::create_issue))
+        .with_state(pool_arc.clone())
+        .route("/issue", get(issue::get_issues))
+        .with_state(pool_arc.clone());
 
     let listener_res = tokio::net::TcpListener::bind("0.0.0.0:3000").await;
     let listener = match listener_res {
