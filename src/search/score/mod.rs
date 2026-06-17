7ba8258172b25e101b67f3458cf6bcf32f10d9dd
IanMGF <ianmarcosgomesefreitas@gmail.com>
2026-06-17 13:37:43 -0300

    feat: add docker compose file

--- src/search/score/mod.rs
+++ src/search/score/mod.rs
@@ -1,7 +1,8 @@
-use std::sync::Arc;
+use std::{fs::File, sync::Arc};
 
 use crate::search::score::layers::{
     ScoringLayer, contains::ContainsScore, fuzzy::FuzzScore, jaccard::JaccardScore,
+    synonym_swap::SynonymSwappingScore,
 };
 
 pub mod layers;
@@ -11,9 +12,13 @@ pub fn attribute_scores<'a>(
     query: &str,
     lines: impl Iterator<Item = &'a Arc<str>>,
 ) -> impl Iterator<Item = f64> {
+    // let synonym_swap_scorer: SynonymSwappingScore =
+    //     yaml_serde::from_reader(File::open("synonyms.txt").unwrap()).unwrap();
+
     let scored = lines.map(Arc::as_ref).map(|line| (line, 0f64));
     let scored = FuzzScore::attribute_score_iter(&FuzzScore, query, scored);
     let scored = ContainsScore::attribute_score_iter(&ContainsScore, query, scored);
     let scored = JaccardScore::attribute_score_iter(&JaccardScore, query, scored);
+    // let scored = synonym_swap_scorer.attribute_score_iter(query, scored);
     scored.map(|(_line, score)| score)
-}
+}   
